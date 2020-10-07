fd2d305883834d37cd50da265d77f47510d6ca12
Young <limyoung116@gmail.com>
2020-10-08 08:04:55 +0900

    Add Approval Button

--- src/Components/TourApporved.js
+++ src/Components/TourApporved.js
@@ -4,27 +4,38 @@ import {AiOutlineLeft} from "react-icons/ai";
 import web3 from "../web3.js";
 import RemainToken from "../RemainToken";
 import { Link } from "react-router-dom";
+import { GiWaxSeal } from "react-icons/gi";
+import Web3 from 'web3';
+
 
  
 export default function Poster({location, description}) {
 
-  const onClick = async (event) => {
-    const accounts = await web3.eth.getAccounts();
-    await RemainToken.methods.verifyUserLocation(accounts[0],{location},"제주도를 따라가다.").send({from:accounts[0]},function(err,res) {
-      alert("인증되었습니다.")
-    })
+  const onClick =  async () => {
+    const web3 = new Web3(window.web3.currentProvider);
+    web3.eth.getAccounts(function(err, accountList) {
+      console.log(err, accountList);
+      RemainToken.methods.verifyUserLocation(accountList[0],{location},"제주도를 따라가다.").send({from:accountList[0]},function(err,res) {
+        alert("인증되었습니다.")
+      })
+
+      })
  }
  
   return (
+    <>
+    <Approve>
+      <GiWaxSeal onClick = {onClick} />
+    </Approve>
     <Main to={`/theme/1/Map`}>
-      <Img src={require('../assets/project3.jpg')} width='340' height='180' />
+      <Img src={require('../assets/project3.jpg')} width='300' height='180' />
       <MainProject>
         <View>
           <First>나의 포스팅</First>
         </View>
         <View>
           <AiOutlineLeft color='white' />
-          <Second onClick = {onClick}>{location}</Second>
+          <Second>{location}</Second>
         </View>
         <View>
           <First>제작 : 배준호</First>
@@ -34,14 +45,25 @@ export default function Poster({location, description}) {
         </View>
       </MainProject>
     </Main>
+    </>
   );
 };
+
+const Approve = styled.span`
+   top: -50px;
+   color: red;
+   font-size: 70px;
+   padding-left: 270px;
+   width: 0px;
+   height: 0px;
+   z-index: 2;
+`
 
 const Main = styled(Link)`
     display: flex;
     flex-direction: row;
     height: 200px;
-    width: 340px;
+    width: 300px;
     margin : 10px;
     margin-bottom : 30px;
     background-color: rgba(0,0,0, 0.4);
